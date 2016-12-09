5be47f7029186f18f2969dac0376923b967e137e
Gregory Dillon <gregorydillon@gmail.com>
2016-12-09 15:05:02 -0500

    video vorking better, too many images

--- graffiti_SF311/pipevid.sh
+++ graffiti_SF311/pipevid.sh
@@ -1,8 +1,8 @@
 cd assets
 
-wget -i ../imagelist.txt
-sleep 15
-%ls | gawk 'BEGIN{ a=1 }{ printf "mv \"%s\" %04d.jpg\n", $0, a++ }' | bash
+#wget -i ../imagelist.txt
+#sleep 15
+ls | gawk 'BEGIN{ a=1 }{ printf "mv \"%s\" %04d.jpg\n", $0, a++ }' | bash
 sleep 5
 convert -delay 77 *.jpg out.mp4
 
