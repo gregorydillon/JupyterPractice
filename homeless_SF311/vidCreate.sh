0518fa4dfd89bab58bb45cc6c23762c856033e4a
Gregory Dillon <gregorydillon@gmail.com>
2016-12-28 21:27:58 -0500

    funtion version"

--- homeless_SF311/vidCreate.sh
+++ homeless_SF311/vidCreate.sh
@@ -1,8 +1,8 @@
 cd input
 rm 0*.jpg
+#wget -i ../heartofCity.txt
 wget -i ../imagelist.txt
 ls | gawk 'BEGIN{ a=1 }{ printf "mv \"%s\" %04d.jpg\n", $0, a++ }' | bash
 
-ffmpeg -framerate 1 -i %04d.jpg ../video.gif
+ffmpeg -framerate 1/2 -i %04d.jpg  ../imagelist.gif
 cd ..
-
--- homeless_SF311/vidMission.sh
+++ homeless_SF311/vidMission.sh
@@ -1,8 +1,28 @@
+rm *.gif
 cd input
 rm 0*.jpg
-wget -i ../imagelist.txt
+wget -i ../mission.txt
 ls | gawk 'BEGIN{ a=1 }{ printf "mv \"%s\" %04d.jpg\n", $0, a++ }' | bash
+ffmpeg -framerate 1/2 -i %04d.jpg ../mission.gif
 
-ffmpeg -framerate 1 -i %04d.jpg ../mission.gif
+
+rm 0*.jpg
+wget -i ../ronan.txt
+ls | gawk 'BEGIN{ a=1 }{ printf "mv \"%s\" %04d.jpg\n", $0, a++ }' | bash
+ffmpeg -framerate 1/2 -i %04d.jpg ../ronan.gif
+
+
+rm 0*.jpg
+wget -i ../soma.txt
+ls | gawk 'BEGIN{ a=1 }{ printf "mv \"%s\" %04d.jpg\n", $0, a++ }' | bash
+ffmpeg -framerate 1/2 -i %04d.jpg ../soma.gif
+
+
+
+rm 0*.jpg
+wget -i ../kim.txt
+ls | gawk 'BEGIN{ a=1 }{ printf "mv \"%s\" %04d.jpg\n", $0, a++ }' | bash
+ffmpeg -framerate 1/2 -i %04d.jpg ../kim.gif
+
 cd ..
 
